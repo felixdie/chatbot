b72e0a523b8c3106e952520b2a1483110ad0b119
Felix Dietrich <felix.a.dietrich@capgemini.com>
2024-11-07 21:33:16 +0100

    deploy V1

--- frontend.py
+++ frontend.py
@@ -5,6 +5,7 @@ from backend import (
     create_retrival_chain,
     get_answer,
     get_logger,
+    Master_Agent,
 )
 import streamlit as st
 from config.ingest_config import config
@@ -18,50 +19,32 @@ session_states = {
     "vectorstore_initialised": False,
     "vectorstore": None,
 }
-
 for session_state, initial_value in session_states.items():
     if session_state not in st.session_state:
         st.session_state[session_state] = initial_value
 
-# Initialise LLM
-llm = initialise_llm()
-
 # Create frontend
 st.title("🔗 GPT Tool for HAI Literature Review")
 
-# Reset and task checkboxes
-col1, col2, col3, col4 = st.columns(4)
 
-with col1:
-    task_1 = st.checkbox("Task 1", value=True)
+# col1, col2, col3, col4 = st.columns(4)
+# with col1:
 
-with col3:
-    task_2 = st.checkbox("Task 2")
-
-with col4:
-
-    # Reset vectorstore and reload frontend
-    reset = st.button("Reset")
-    if reset:
-        if st.session_state["vectorstore"] is not None:
-            st.session_state["vectorstore"].delete_collection()
-        st.session_state["vectorstore_initialised"] = False
-        logger.info("Vectorstore cleared")
-        st.rerun()
+# Reset vectorstore and reload frontend
+# reset = st.button("Reset")
+# if reset:
+#     if st.session_state["vectorstore"] is not None:
+#         st.session_state["vectorstore"].delete_collection()
+#     st.session_state["vectorstore_initialised"] = False
+#     logger.info("SUCCESS: Vectorstore cleared")
+#     st.rerun()
 
 # Input form
 with st.form("my_form"):
-    if task_1:
-        user_input = st.text_area(
-            "Enter a reference:",
-            placeholder="Yalcin, G., Lim, S., Puntoni, S., & van Osselaer, S. M. J. (2022). Thumbs Up or Down: Consumer Reactions to Decisions by Algorithms Versus Humans",
-        )
-
-    elif task_2:
-        user_input = st.text_area(
-            "Enter your question:",
-            placeholder="Do a literature review",
-        )
+    user_input = st.text_area(
+        "Enter your question:\n",
+        placeholder="Summarise AI in Business Research by Cao et al. (2024) \nConduct a literature review on Conceptual/ Case Studies about human + AI\n...",
+    )
 
     # Submit button
     submitted = st.form_submit_button("Submit")
@@ -72,13 +55,19 @@ with st.form("my_form"):
     # Output field
     if submitted:
 
+        # Initialise master agent
+        master_agent = Master_Agent()
+
+        # Determine task
+        task = master_agent.choose_task(user_input=user_input)
+
+        # Initialise LLM
+        llm = initialise_llm(task=task)
+
         # Initialise vectorstore only once
         if st.session_state["vectorstore_initialised"] == False:
             st.session_state["vectorstore"] = preprocess_data(
-                task_1=task_1,
-                task_2=task_2,
-                user_input=user_input,
-                llm=llm,
+                task=task, user_input=user_input
             )
             st.session_state["vectorstore_initialised"] = True
 
@@ -86,17 +75,21 @@ with st.form("my_form"):
             vectorstore=st.session_state["vectorstore"],
             llm=llm,
             query=user_input,
-            task_1=task_1,
-            task_2=task_2,
+            task=task,
         )
+        logger.info("SUCCESS: RAG initialised")
 
         retrival_chain = create_retrival_chain(
-            llm=llm,
-            query_transformer=query_transformer,
-            task_1=task_1,
-            task_2=task_2,
+            llm=llm, query_transformer=query_transformer, task=task
         )
+        logger.info("SUCCESS: Retrival Chain initialised")
 
         # Get answer
         answer = get_answer(query=user_input, query_chain=retrival_chain)
+        logger.info("SUCCESS: Answer generated")
         st.info(answer)
+
+        if st.session_state["vectorstore"] is not None:
+            st.session_state["vectorstore"].delete_collection()
+            st.session_state["vectorstore_initialised"] = False
+            logger.info("SUCCESS: Vectorstore cleared")
